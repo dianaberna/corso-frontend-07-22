05f3de3884b4ebc0515250d60cfb81cd879e7445
Paola Collorafi <paolacollorafi@gmail.com>
2022-07-06 15:54:41 +0200

    feat: new exercise JS

--- eserciziJS/script.js
+++ eserciziJS/script.js
@@ -1,13 +1,28 @@
-let element = document.getElementById("container");
-element.style.backgroundColor = "black";
+let containerElement = document.getElementById("container");
+containerElement.style.backgroundColor = "black";
 
-let paragrafoGenerato = document.createElement("p");
+let generatedParagraph = document.createElement("p");
 
 let testo = document.createTextNode("Aiuto, spegni la luce! :£");
-paragrafoGenerato.appendChild(testo);
-element.appendChild(paragrafoGenerato);
+generatedParagraph.appendChild(testo);
+containerElement.appendChild(generatedParagraph);
+
+let paragraphElement = document.getElementById("paragraph");
 
 function clickTheButton(clickon){
     console.log("funziono!");
-    paragrafoGenerato.style.color = "orange";
+    if(generatedParagraph.style.color == "black") {
+        generatedParagraph.style.color = "orange"; 
+    } else {
+        generatedParagraph.style.color = "black";
+    } 
+}
+
+function clickForm(){
+    console.log("ciao");
+    let nome = document.getElementById("nome");
+    let cognome = document.getElementById("cognome");
+    console.log(nome.value);
+    console.log(cognome.value);
+    paragraphElement.innerHTML = nome.value + " " + cognome.value;
 }
