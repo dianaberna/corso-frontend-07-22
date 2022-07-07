f2d59a3e70c1b701216890d98a3aec4523889ff9
Silvio <silvio.salvatierra.it@gmail.com>
2022-07-07 17:26:17 +0200

    esercizio

--- todoList.js
+++ todoList.js
@@ -1,9 +1,57 @@
+// function aggElem(){
+//     let lista = document.getElementById('lista');
+//     let elemento = document.getElementById('elemento').value;
+//     console.log(elemento);
+//     let nuovoElem = document.createElement('li');
+//     nuovoElem.innerHTML = elemento;
+//     let id = document.getElementsByTagName('li').length +1;
+//     nuovoElem.id = id
+
+//     let btnElimina = document.createElement('button');
+//     btnElimina.innerHTML='elimina';
+//     btnElimina.type = 'button';
+//     btnElimina.name = 'elimina';
+//     btnElimina.onclick = function(){
+//         return Elimina(id)
+//     }
+    
+//     nuovoElem.appendChild(btnElimina);
+//     lista.appendChild(nuovoElem);
+// }
+
+// function Elimina(elemento){
+//     let togli = document.getElementById(elemento);
+//     togli.parentNode.removeChild(togli);
+//     return;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+let carrello = [];
+
 function aggElem(){
+
     let lista = document.getElementById('lista');
     let elemento = document.getElementById('elemento').value;
     console.log(elemento);
-    let nuovoElem = document.createElement('li');
-    nuovoElem.innerHTML = elemento;
+    nuovoElem = document.createElement('li');
+
+    nuovoElem.innerHTML= elemento
+    carrello.push(nuovoElem);
+    console.log('---------' + carrello);
+    
+    // nuovoElem.innerHTML = elemento;
+
     let id = document.getElementsByTagName('li').length +1;
     nuovoElem.id = id
 
@@ -24,4 +72,3 @@ function Elimina(elemento){
     togli.parentNode.removeChild(togli);
     return;
 }
-
