c2c26e49d38b793979f3f5e0590f532a9d881f50
Paola Collorafi <paolacollorafi@gmail.com>
2022-07-07 17:38:08 +0200

    feat: new feature to do list

--- eserciziJS/script.js
+++ eserciziJS/script.js
@@ -1,18 +1,17 @@
+let elementIndex = 0;
+
 let containerElement = document.getElementById("container");
 containerElement.style.backgroundColor = "black";
 
 let generatedParagraph = document.createElement("p");
-
 let testo = document.createTextNode("Aiuto, spegni la luce! :£");
 generatedParagraph.appendChild(testo);
 containerElement.appendChild(generatedParagraph);
 
-let paragraphElement = document.getElementById("paragraph");
-
 let form = document.getElementById("containerForm");
 
-let buttonText = document.getElementById("lightText");
 
+let buttonText = document.getElementById("lightText");
 let isLightTurnedOn = false;
 
 function clickTheButton() {
@@ -28,7 +27,9 @@ function clickTheButton() {
     isLightTurnedOn = !isLightTurnedOn;
 }
 
-function clickForm(){
+let paragraphElement = document.getElementById("paragraph");
+
+function clickForm() {
     console.log("ciao");
     let nome = document.getElementById("nome");
     let cognome = document.getElementById("cognome");
@@ -36,3 +37,34 @@ function clickForm(){
     console.log(cognome.value);
     paragraphElement.innerHTML = nome.value + " " + cognome.value;
 }
+
+function addList() {
+
+    let list = document.getElementById("list");
+    let listElement = document.getElementById("listElement").value;
+    console.log(listElement);
+
+    let id = elementIndex;
+    elementIndex++;
+
+    let newElement = document.createElement("li");
+    newElement.id = id;
+    newElement.innerHTML = listElement;
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "delete";
+    deleteButton.type = "button";
+    deleteButton.name = "delete";
+    deleteButton.className = "deleteListButton";
+    deleteButton.onclick = () => deleteListElement(id);
+    newElement.appendChild(deleteButton);
+
+    list.appendChild(newElement);
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+function deleteListElement(id) {
+    let elementToDelete = document.getElementById(id);
+    elementToDelete.parentNode.removeChild(elementToDelete);
+}
+
