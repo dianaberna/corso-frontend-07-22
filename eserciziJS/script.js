1bf5fabbe99f8b407296d84b3ecfc6bd308ee697
Paola Collorafi <paolacollorafi@gmail.com>
2022-07-08 12:16:10 +0200

    style: lint

--- eserciziJS/script.js
+++ eserciziJS/script.js
@@ -10,61 +10,58 @@ containerElement.appendChild(generatedParagraph);
 
 let form = document.getElementById("containerForm");
 
-
 let buttonText = document.getElementById("lightText");
 let isLightTurnedOn = false;
 
 function clickTheButton() {
-    if (isLightTurnedOn) {
-        generatedParagraph.style.color = "black";
-        form.style.display = "none";
-        document.getElementById("lightText").innerHTML = "accendi la luce";
-    } else {
-        generatedParagraph.style.color = "orange"; 
-        form.style.display = "flex";
-        document.getElementById("lightText").innerHTML = "💡 spegni la luce 💡";
-    }
-    isLightTurnedOn = !isLightTurnedOn;
+  if (isLightTurnedOn) {
+    generatedParagraph.style.color = "black";
+    form.style.display = "none";
+    document.getElementById("lightText").innerHTML = "accendi la luce";
+  } else {
+    generatedParagraph.style.color = "orange";
+    form.style.display = "flex";
+    document.getElementById("lightText").innerHTML = "💡 spegni la luce 💡";
+  }
+  isLightTurnedOn = !isLightTurnedOn;
 }
 
 let paragraphElement = document.getElementById("paragraph");
 
 function clickForm() {
-    console.log("ciao");
-    let nome = document.getElementById("nome");
-    let cognome = document.getElementById("cognome");
-    console.log(nome.value);
-    console.log(cognome.value);
-    paragraphElement.innerHTML = nome.value + " " + cognome.value;
+  console.log("ciao");
+  let nome = document.getElementById("nome");
+  let cognome = document.getElementById("cognome");
+  console.log(nome.value);
+  console.log(cognome.value);
+  paragraphElement.innerHTML = nome.value + " " + cognome.value;
 }
 
 function addList() {
+  let list = document.getElementById("list");
+  let listElement = document.getElementById("listElement").value;
+  console.log(listElement);
 
-    let list = document.getElementById("list");
-    let listElement = document.getElementById("listElement").value;
-    console.log(listElement);
+  let id = elementIndex;
+  elementIndex++;
 
-    let id = elementIndex;
-    elementIndex++;
+  let newElement = document.createElement("li");
+  newElement.id = id;
+  newElement.innerHTML = listElement;
 
-    let newElement = document.createElement("li");
-    newElement.id = id;
-    newElement.innerHTML = listElement;
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "delete";
+  deleteButton.type = "button";
+  deleteButton.name = "delete";
+  deleteButton.className = "deleteListButton";
+  deleteButton.onclick = () => deleteListElement(id);
+  newElement.appendChild(deleteButton);
 
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "delete";
-    deleteButton.type = "button";
-    deleteButton.name = "delete";
-    deleteButton.className = "deleteListButton";
-    deleteButton.onclick = () => deleteListElement(id);
-    newElement.appendChild(deleteButton);
-
-    list.appendChild(newElement);
-    window.scrollTo(0, document.body.scrollHeight);
+  list.appendChild(newElement);
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 function deleteListElement(id) {
-    let elementToDelete = document.getElementById(id);
-    elementToDelete.parentNode.removeChild(elementToDelete);
+  let elementToDelete = document.getElementById(id);
+  elementToDelete.parentNode.removeChild(elementToDelete);
 }
-
